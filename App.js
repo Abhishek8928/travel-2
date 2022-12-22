512ad568a8d044395a6ceb98ef212a18b31134fb
Abhishek Sharma <abhishekshaxma8356@gmail.com>
2022-12-22 13:48:06 +0530

    v3

--- App.js
+++ App.js
@@ -14,17 +14,3 @@ let openNavigationHandler = () => {
     }
 }
 
-// js for form
-
-
-let selectLocation = () => {
-
-    let userlocation = document.getElementById("location").value;
-    let locationInput = document.getElementsByClassName("hero-input")[0];
-    locationInput.value = userlocation;
-}
-let selectPeople = () => {
-    let optionValue = document.getElementById("no-people").value;
-    let peopleInput = document.getElementsByClassName("hero-input")[2];
-    peopleInput.value = optionValue;
-}
